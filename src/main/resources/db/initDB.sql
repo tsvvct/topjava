200181689789fe9183282eec0e770e9f4d6b247c
tsv <victor.tsvikevich@gmail.com>
2021-10-23 13:02:49 +0300

    HW03 complete

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -23,3 +24,16 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    dateTime         TIMESTAMP           DEFAULT now() NOT NULL,
+    description      VARCHAR                           NOT NULL,
+    calories         INTEGER                           NOT NULL,
+    user_id INTEGER NOT NULL,
+    CONSTRAINT user_meals_date UNIQUE (user_id, dateTime),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX meals_ids ON meals(user_id, id);
