adbb4d040bcd4ff408a38e5dba1c542d5873abd8
tsv <victor.tsvikevich@gmail.com>
2021-11-19 01:06:03 +0300

    HW06 optional user roles manged with jdbc

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,22 +1,53 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
+@Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+    private static final String baseUserSelectSql = """
+            SELECT u.*, ur.role as roles FROM users u
+            LEFT JOIN user_roles ur on u.id = ur.user_id
+            """;
+
+    private ResultSetExtractor<List<User>> resultSetExtractor = new ResultSetExtractor<>() {
+        @Override
+        public List<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            Map<Integer, User> userMap = new HashMap<>();
+
+            while (rs.next()) {
+                User user = ROW_MAPPER.mapRow(rs, rs.getRow());
+                if (user.getRoles() == null) {
+                    user.setRoles(Collections.emptyList());
+                }
+                userMap.merge(user.getId(), user, (mappedUser, newUser) -> {
+                    mappedUser.getRoles().addAll(newUser.getRoles());
+                    return mappedUser;
+                });
+            }
+            return List.copyOf(userMap.values());
+        }
+    };
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -35,41 +66,66 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
+        String rolesInsertSql = "INSERT INTO user_roles (user_id, role) values (:user_id, :role)";
 
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
+            List<MapSqlParameterSource> rolesParams = extracted(user.getRoles(), newKey.intValue());
+            namedParameterJdbcTemplate.batchUpdate(rolesInsertSql, rolesParams.toArray(MapSqlParameterSource[]::new));
             user.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update("""
-                   UPDATE users SET name=:name, email=:email, password=:password, 
-                   registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
-                """, parameterSource) == 0) {
-            return null;
+        } else {
+            if (namedParameterJdbcTemplate.update("""
+                       UPDATE users SET name=:name, email=:email, password=:password, 
+                       registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
+                    """, parameterSource) == 0) {
+                return null;
+            } else {
+                namedParameterJdbcTemplate.update("DELETE FROM user_roles WHERE user_id=:id", parameterSource);
+                if (user.getRoles().size() > 0) {
+                    List<MapSqlParameterSource> rolesParams = extracted(user.getRoles(), user.getId());
+                    namedParameterJdbcTemplate.batchUpdate(rolesInsertSql, rolesParams.toArray(MapSqlParameterSource[]::new));
+                }
+            }
         }
         return user;
     }
 
+    private List<MapSqlParameterSource> extracted(Set<Role> userRoles, int userId) {
+        List<MapSqlParameterSource> params = new ArrayList<MapSqlParameterSource>();
+
+        for (Role role : userRoles) {
+            MapSqlParameterSource source = new MapSqlParameterSource();
+            source.addValue("user_id", userId);
+            source.addValue("role", role.toString());
+            params.add(source);
+        }
+        return params;
+    }
+
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        List<User> users = jdbcTemplate.query(baseUserSelectSql + " WHERE id=?", resultSetExtractor, id);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
+        List<User> users = jdbcTemplate.query(baseUserSelectSql + " WHERE email=?", resultSetExtractor, email);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return jdbcTemplate.query(baseUserSelectSql + " ORDER BY name, email", resultSetExtractor);
     }
 }
